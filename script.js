c2c3b5da824d8f23a5ddda05794175436678758c
Arlin Schaffel <arlin@fexd.com>
2023-03-10 13:35:22 -0600

    Styling and Select Square
    - only one square can be selected at a time
    - add more color variables for consistency
    - remove svg test

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     console.log(event);
+
+    const board = document.getElementById('board');
+    const squares = board.querySelectorAll('div');
+    squares.forEach( square => {
+        square.addEventListener('click', function() {
+            console.log(`${square.id} was clicked!`);
+            if(square.classList.contains('selected')) {
+                unselectAll();
+            } else {
+                unselectAll();
+                square.classList.add('selected');
+            }
+        });
+    });
+
+    const unselectAll = () => {
+        squares.forEach( square => {
+            square.classList.remove('selected');
+        });
+    };
+
 });
