ae189a74177aee33bc0121f0d456b62fbcd02ff4
Arlin Schaffel <arlin@fexd.com>
2023-04-21 15:22:46 -0600

    Maybe a Better LoadBoard
    - using regular expressions (which are the devil) to modify FEN string
    - allows for easier loop to create squares
    - I kind of hate this solutio as well, but will think on it

--- script.js
+++ script.js
@@ -49,32 +49,24 @@ function loadBoard(fen) {
     const fenPositions = ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8', 'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7', 'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6', 'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5', 'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4', 'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3', 'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2', 'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'];
 
     const fenArr = fen.split(' ');
-    const [piecePlacement, sideToMove,
-        castlingAbility, enPassantTarget,
-        halfMoveClock, fullMoveCounter] = fenArr;
+    const piecePlacement = fenArr[0];
 
     clearBoard();
 
-    let i = 0;
-    let b = 0;
-
-    // Place pieces on board
-    // TODO: refactor this
-    while (i < piecePlacement.length) {
-        if (piecePlacement[i] === '/') {
-            i++;
-            continue;
+    // replace numbers with spaces and remove forward slashes
+    let newPiecePlacement = piecePlacement.replace(/[0-8/]/g, (match) => {
+        if (match === '/') {
+            return ''; // remove forward slash
+        } else {
+            return " ".repeat(parseInt(match)); // repeat " " for the number of times matched
         }
+    });
 
-        if (parseInt(piecePlacement[i])) {
-            b += parseInt(piecePlacement[i]);
-            i++;
-            continue;
+    // add pieces to board
+    for (let i = 0; i < newPiecePlacement.length; i++) {
+        const square = document.getElementById(fenPositions[i]);
+        if (newPiecePlacement[i] !== ' ') {
+            square.classList.add(newPiecePlacement[i]);
         }
-        
-        const square = document.getElementById(fenPositions[b]);
-        square.classList.add(piecePlacement[i]);
-        i++;
-        b++;
     }
 }
