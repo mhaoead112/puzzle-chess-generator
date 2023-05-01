3d523d11334eedbea41dd67ceae7277cbaae88fa
Arlin Schaffel <arlin@fexd.com>
2023-05-01 10:51:20 -0600

    Playable Game!
    - messy and needs some refactoring, but working
    - could store state better, but trying to avoid overcomplicating
    - remove old board setup with fen only
    - flipping logic changed, as puzzle FEN is loaded one move prior to player move

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ import { move, status, getFen }  from './js-chess-engine/lib/js-chess-engine.mjs
 
 const fenPositions = ['a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8', 'h8', 'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7', 'a6', 'b6', 'c6', 'd6', 'e6', 'f6', 'g6', 'h6', 'a5', 'b5', 'c5', 'd5', 'e5', 'f5', 'g5', 'h5', 'a4', 'b4', 'c4', 'd4', 'e4', 'f4', 'g4', 'h4', 'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'h3', 'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2', 'a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1'];
 const pieces = ['r', 'n', 'b', 'q', 'k', 'p', 'R', 'N', 'B', 'Q', 'K', 'P'];
+const speed = 200;
+
+let puzzle_solved = false;
 let currentPuzzle = '';
 let currentFEN = '';
 let currentStatus = '';
+let lastPuzzleMoveIndex = 0;
 
 window.addEventListener("DOMContentLoaded", (event) => {
+    setUpBoard();
+    loadRandomPuzzle();
+});
+
+function setUpBoard() {
     document.getElementById("no-js").remove();
 
     const board = document.getElementById('board');
@@ -20,6 +29,9 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     const squareClicked = (square) => {
         console.log(`${square} was clicked!`);
+        if (puzzle_solved) {
+            return;
+        }
         // You may be temped to refactor, but why?
         // unselectAll is catching **edge case** for multiple selected squares
         // you still need to unselect the selected square, if user wants to undo selection
@@ -28,7 +40,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         } else if (square.classList.contains('circle')) {
             const selected = document.querySelector('.selected');
             unselectAll();
-            movePiece(selected.id, square.id);
+            playerMove(selected.id, square.id);
         } else {
             unselectAll();
 
@@ -47,19 +59,19 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
     }
 
-    const unselectAll = () => {
-        squares.forEach(square => {
-            square.classList.remove('selected');
-            square.classList.remove('circle');
-        });
-    };
-
-    document.getElementById('random-fen').addEventListener('click', () => {
-        loadRandomFen();
+    document.getElementById('random-puzzle').addEventListener('click', () => {
+        loadRandomPuzzle();
     });
+}
 
-    loadPuzzle();
-});
+function unselectAll() {
+    const board = document.getElementById('board');
+    const squares = board.querySelectorAll('div');
+    squares.forEach(square => {
+        square.classList.remove('selected');
+        square.classList.remove('circle');
+    });
+}
 
 function clearBoard() {
     const board = document.getElementById('board');
@@ -69,7 +81,7 @@ function clearBoard() {
     });
 }
 
-function loadBoard(fen, shouldFlip = false) {
+function loadBoard(fen) {
     const fenArr = fen.split(' ');
     const piecePlacement = fenArr[0];
 
@@ -91,16 +103,14 @@ function loadBoard(fen, shouldFlip = false) {
             square.classList.add(newPiecePlacement[i]);
         }
     }
+}
 
+function flipBoard(shouldFlip) {
     const board = document.getElementById('board');
-
-    // flip board if black's turn and shouldFlip is true
     if (shouldFlip) {
-        if(currentStatus.turn === 'black') {
-            board.classList.add('flip');
-        } else {
-            board.classList.remove('flip');
-        }
+        board.classList.add('flip');
+    } else {
+        board.classList.remove('flip');
     }
 }
 
@@ -129,55 +139,79 @@ function selectPiece(element) {
     }
 }
 
+function computerMove(from, to) {
+    // wait 1 second then move piece
+    setTimeout(() => {
+        movePiece(from, to);
+    }, speed);
+}
+
+function playerMove(from, to) {
+    console.log(lastPuzzleMoveIndex);
+    console.log(currentPuzzle.moves[lastPuzzleMoveIndex]);
+    console.log(`${from}${to}`);
+    if(currentPuzzle.moves[lastPuzzleMoveIndex+1] === `${from}${to}`) {
+        lastPuzzleMoveIndex++;
+        puzzleMoveGood(from, to);
+    } else {
+        puzzleMoveBad(from, to);
+    }
+}
+
+function puzzleMoveGood(from, to) {
+    movePiece(from, to);
+    lastPuzzleMoveIndex++;
+    if(lastPuzzleMoveIndex >= currentPuzzle.moves.length) {
+        updateMessage('✅ Puzzle complete!', 'good');
+        puzzle_solved = true;
+    } else {
+        updateMessage('Good move, keep going.', 'good');
+        setTimeout(() => {
+            
+            computerMove(currentPuzzle.moves[lastPuzzleMoveIndex].substring(0, 2), currentPuzzle.moves[lastPuzzleMoveIndex].substring(2, 4));
+        }, speed);
+    }
+}
+
+function puzzleMoveBad(from, to) {
+    const backupStatus = currentFEN;
+    const backupPrevious = document.querySelectorAll('.previous');
+    console.log(backupPrevious);
+    movePiece(from, to);
+    updateMessage('❌ There are better moves, try again.', 'bad');
+    setTimeout(() => {
+        loadFen(backupStatus);
+        backupPrevious.forEach(element => {
+            element.classList.add('previous');
+        });
+    }, speed);
+}
+
 function movePiece(from, to) {
     console.log(`move ${from} to ${to}`);
     currentStatus = move(currentStatus, from.toUpperCase(), to.toUpperCase());
     currentFEN = getFen(currentStatus);
     loadBoard(currentFEN);
+    document.getElementById(from).classList.add('previous');
+    document.getElementById(to).classList.add('previous');
 }
 
-function loadRandomFen() {
-    const fenPositions = [
-        "r1bqk2r/ppp2ppp/2n1pn2/1B1p4/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 4",
-        "rnbqk2r/pppp1ppp/4p3/8/2B1P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 0 3",
-        "r1bq1rk1/pppnbppp/4p3/3pP3/2P5/2N2N2/PP2BPPP/R1BQK2R b KQ - 3 7",
-        "r3k2r/1p1b1ppp/p1nppn2/8/2P5/1PN1PN2/P1B1BPPP/R3K2R w KQkq - 0 11",
-        "r2qkb1r/pp1n1ppp/2p1pn2/2Pp4/3P1B2/2N2N2/PP3PPP/R2QKB1R b KQkq - 1 7",
-        "r1bqk2r/1p2bppp/p1n1pn2/2pp4/2P5/2N1PN2/PP1P1PPP/R1BQKB1R w KQkq - 0 7",
-        "rnbqkb1r/pppp1ppp/4p3/8/3Pn3/2N5/PPP1PPPP/R1BQKBNR w KQkq - 1 3",
-        "r1bq1rk1/p3bppp/1p2pn2/8/2PP4/1PN1PN2/P3BPPP/R2QK2R w KQ - 2 11",
-        "r1b1k2r/pp2bppp/2n1pn2/q7/2p1P3/2N1BN2/PP1QBPPP/R3K2R b KQkq - 0 10",
-        "r1b1k2r/pp1p1ppp/1q3n2/3n4/2B1P3/2N5/PPP2PPP/R1BQK2R w KQkq - 2 7",
-        "r2qkb1r/pp1n1ppp/2pbpn2/3p4/2PP4/2N1PN2/PP3PPP/R1BQKB1R w KQkq - 0 7",
-        "rnbqk2r/pp2nppp/2p1p3/3p4/2PP4/5N2/PP2PPPP/RNBQKB1R w KQkq - 0 4",
-        "r1bqkb1r/1p2nppp/p1np4/4p3/2PP4/2N2N2/PP3PPP/R1BQKB1R w KQkq - 0 6",
-        "r1b1k2r/pp2bppp/2n1p3/q7/2Pp4/2N1PN2/PP3PPP/R1BQKB1R w KQkq - 0 9",
-        "rnbqk2r/pppp1ppp/4p3/8/2P5/5N2/PP1PPPPP/RNBQKB1R b KQkq - 0 2",
-        "rnbqk2r/pppp1ppp/4p3/8/2P1n3/5N2/PP1PPPPP/RNBQKB1R w KQkq - 1 3",
-        "r1b1kb1r/p1pp1ppp/1q2pn2/n7/2P5/2N2N2/PP1PPP1P/R1BQKB1R w KQkq - 1 6",
-        "r1b1kb1r/p1pp1ppp/1q2pn2/8/2P5/2N1PN2/PP1P1PPP/R1BQKB1R w KQkq - 0 6",
-        "r1bqk2r/pp2nppp/2pbpn2/3p4/2PP4/2N1PN2/PP3PPP/R1BQKB1R b KQkq - 0 7",
-        "r1b1k1nr/pp1p1ppp/2n1p3/1B6/2P5/5N2/PP1PPPPP/RNBQK2R b KQkq - 0 3",
-        "rnbqk2r/ppp1nppp/3p4/4p3/2P5/5N2/PP1PPPPP/RNBQKB1R w KQkq - 0 3",
-        "rnbqk2r/ppp1nppp/3p4/4p3/2P1P3/5N2/PP1P1PPP/RNBQKB1R b KQkq - 0 3",
-        "r1bqkb1r/pp2nppp/2n1p3/1B1p4/4P3/2N2N2/PPPP1PPP/R1BQK2R b KQkq - 3 5",
-        "r1b1k2r/pp2bppp/2n2n2/q1p1p3/3pP3/2PP1N1P/P1QB1PP1/RN3RK1 w kq - 0 11",
-        "r1bqk2r/ppp1bppp/2n5/3p4/2PPn3/2N1PN2/PP3PPP/R1BQKB1R w KQkq - 0 6",
-        "1r1r2k1/2q2pbp/ppp1p1p1/n7/2PP4/1PN2P2/P2Q2PP/2R2RK1 w - - 0 19",
-        "r1bq1rk1/pp3pbp/2np1np1/2p5/2P5/2NPPN2/PP3PPP/R1BQ1RK1 w - - 0 9",
-        "rnbqkbnr/pppp2pp/4p3/5p2/2PP4/5N2/PP2PPPP/RNBQKB1R w KQkq - 0 3",
-        "r3r1k1/1p3ppp/2b1p3/p2n4/2P5/PP3P2/3R2PP/R1B3K1 w - - 0 21",
-        "rnbqk2r/pp2bppp/2p2n2/3p4/2PP4/2N1PN2/PP3PPP/R1BQKB1R w KQkq - 0 7",
-        "r1b2rk1/pp2qppp/2n1p3/3pP3/3P1P2/2N1B3/PP3QPP/R4RK1 w - - 0 16",
-        "r1bqk2r/ppp2ppp/2n5/2b5/4P3/2N5/PPP2PPP/R1BQKBNR w KQkq - 0 4",
-        "r1b1k2r/2pp1ppp/p1pb1n2/q7/2PP4/P1N1PN2/1P3PPP/R1BQ1RK1 w kq - 0 11",
-        "r3r1k1/ppp3pp/3bpn2/3p1p2/2PP4/1P3P2/P2N2PP/2BRR1K1 w - - 0 20",
-        "rnbqkb1r/ppp1pppp/5n2/3p4/2P5/5NP1/PP1PPP1P/RNBQKB1R w KQkq - 0 3",
-        "r1b2rk1/pp1n1ppp/2pbpn2/q7/2PP4/P1N1PN2/1P3PPP/R1BQR1K1 w - - 0 14",
-    ];
+function loadRandomPuzzle() {
+    const puzzles = [
+        ["8/R7/3P4/4p1p1/3rPp1k/5P2/5K2/8 b - - 0 46","d4d6 a7h7 d6h6 h7h6",495],
+        ["8/4p2k/3p2p1/3q3p/7P/2Q1P1P1/5P2/6K1 b - - 1 45","h7h6 c3h8",460],
+        ["3r4/pB3R2/1p2p3/8/kP4b1/2P1B3/P4P2/4K3 w - - 1 29","b7e4 d8d1",415],
+        ["4rk2/ppp1rp1p/6p1/5Q2/3p4/P2P2R1/KPP5/6R1 b - - 0 25","g6f5 g3g8",406],
+        ["6k1/1N3ppp/p1p2n2/4p3/1r6/4P1P1/5P1P/3R2K1 b - - 0 29","b4b7 d1d8 f6e8 d8e8",400],
+        ["1r4k1/5p1p/1R3Pp1/1p6/8/7P/6P1/3R2K1 b - - 0 31","b8b6 d1d8",400],
+        ["8/R7/5p2/P5p1/4r3/4r1k1/6P1/3R2K1 w - - 16 42","a7f7 e3e1 d1e1 e4e1",464],
+        ["6k1/p5p1/1p4rp/3p4/5p2/P1P4r/1P3qP1/3QN1RK w - - 0 37","g2h3 f2g1",496],
+        ["6k1/pp3rp1/6P1/8/8/P2R4/1KP5/8 b - - 0 30","f7c7 d3d8",400],
+        ["3rr1k1/p3pp1p/1p3QpB/2p5/2P1R3/3q3P/P4PP1/4R1K1 b - - 0 22","e7f6 e4e8 d8e8 e1e8",424]
+      ];
 
-    const random_fen = fenPositions[Math.floor(Math.random() * fenPositions.length)]
-    loadFen(random_fen);
+    const random_puzzle = puzzles[Math.floor(Math.random() * puzzles.length)]
+    loadPuzzle(random_puzzle);
 }
 
 function updateMessage(text, type = '') {
@@ -190,15 +224,27 @@ function loadFen(fen) {
     if (validateFEN(fen)) {
         currentFEN = fen;
         currentStatus = status(currentFEN);
-        loadBoard(fen, true);
-        updateMessage(`Find the best move for ${currentStatus.turn}.`);
+        loadBoard(fen);
     } else {
         console.log('invalid FEN');
     }
 }
 
-function loadPuzzle() {
-    const puzzle = ["8/R7/3P4/4p1p1/3rPp1k/5P2/5K2/8 b - - 0 46","d4d6 a7h7 d6h6 h7h6","495"];
-    currentPuzzle = puzzle;
-    loadFen(puzzle[0]); 
+function loadPuzzle(puzzle) {
+    puzzle_solved = false;
+    currentPuzzle = {
+        "fen": puzzle[0],
+        "moves": puzzle[1].split(' '),
+        "rating": puzzle[2],
+    };
+    loadFen(currentPuzzle.fen); 
+    if(currentStatus.turn === 'white') {
+        updateMessage('Find the best move for black.');
+        flipBoard(true);
+    } else {
+        updateMessage('Find the best move for white.');
+        flipBoard(false);
+    }
+    computerMove(currentPuzzle.moves[0].substring(0, 2), currentPuzzle.moves[0].substring(2, 4));
+    lastPuzzleMoveIndex = 0;
 }
